610975c50f223c9a51023d40317418570194c62b
agadilkhan <a_agadilkhan@kbtu.kz>
2023-10-26 20:57:57 +0600

    finish hw9

--- lecture9/internal/repository/pg/user.go
+++ lecture9/internal/repository/pg/user.go
@@ -21,3 +21,33 @@ func (p *Postgres) GetUserByUsername(ctx context.Context, username string) (*ent
 
 	return &u, res.Error
 }
+
+func (p *Postgres) GetUsers(ctx context.Context) (*[]entity.User, error) {
+	var u *[]entity.User
+
+	res := p.DB.WithContext(ctx).Find(&u)
+
+	return u, res.Error
+}
+
+func (p *Postgres) UpdateUser(ctx context.Context, u *entity.User) (*entity.User, error) {
+	var oldUser *entity.User
+
+	p.DB.WithContext(ctx).Where("id=", u.ID).Find(oldUser)
+
+	oldUser = u
+
+	res := p.DB.WithContext(ctx).Save(oldUser)
+
+	return oldUser, res.Error
+}
+
+func (p *Postgres) DeleteUser(ctx context.Context, id uint) (uint, error) {
+	res := p.DB.WithContext(ctx).Delete(&entity.User{}, id)
+
+	if res.Error != nil {
+		return 0, res.Error
+	}
+
+	return id, nil
+}
--- lecture9/internal/repository/repository.go
+++ lecture9/internal/repository/repository.go
@@ -8,10 +8,17 @@ import (
 type Repository interface {
 	CreateUser(ctx context.Context, u *entity.User) (uint, error)
 	GetUserByUsername(ctx context.Context, username string) (*entity.User, error)
+	GetUsers(ctx context.Context) (*[]entity.User, error)
+	UpdateUser(ctx context.Context, u *entity.User) (*entity.User, error)
+	DeleteUser(ctx context.Context, id uint) (uint, error)
 	CreateOrder(ctx context.Context, o *entity.Order) (uint, error)
 	GetOrders(ctx context.Context) (*[]entity.Order, error)
 	GetOrderByID(ctx context.Context, di uint) (*entity.Order, error)
 	DeleteOrder(ctx context.Context, id uint) (uint, error)
 	UpdateOrder(ctx context.Context, o *entity.Order) (*entity.Order, error)
 	GetProducts(ctx context.Context) (*[]entity.Product, error)
+	GetProductByID(ctx context.Context, id uint) (*entity.Product, error)
+	CreateProduct(ctx context.Context, pr *entity.Product) (uint, error)
+	UpdateProduct(ctx context.Context, pr *entity.Product) (*entity.Product, error)
+	DeleteProduct(ctx context.Context, id uint) (uint, error)
 }
